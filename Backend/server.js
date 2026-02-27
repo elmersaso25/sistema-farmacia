fa48c5b1bedae088e709884a4ad2cbf0847497fe
elmersaso25 <esasor@miumg.edu.gt>
2026-02-26 22:58:28 -0600

    Ajuste de cors en server Backend

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,11 @@ const compraRoute = require('./routes/compraRoute');
 const app = express();
 
 //Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 
