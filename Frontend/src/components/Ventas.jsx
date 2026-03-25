67574812eaf42f328c466d7e92b58725e929c4d1
Elmer Saso Reyes <esasor@miumg.edu.gt>
2026-03-25 17:30:58 -0600

    Mejoras en vista o modulo Ventas

--- Frontend/src/components/Ventas.jsx
+++ Frontend/src/components/Ventas.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import DataTable from "react-data-table-component";
 import Swal from "sweetalert2";
@@ -7,14 +7,18 @@ import Modal from "../components/Modal";
 import { FaEye } from "react-icons/fa";
 import { BsXCircle } from "react-icons/bs";
 
-function Ventas(){
+function Ventas() {
     const [ventas, setVentas] = useState([]);
-        const [loading, setLoading] = useState(true);
-        const [filterText, setFilterText] = useState("");
-        const navigate = useNavigate();
+    const [ventaSeleccionada, setVentaSeleccionada] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [filterText, setFilterText] = useState("");
+    const navigate = useNavigate();
+
+    const [mostrarModal, setMostrarModal] = useState(false);
+    const [detallesVenta, setDetallesVenta] = useState([]);
 
 
-         const formatearFecha = (fecha) => {
+    const formatearFecha = (fecha) => {
         if (!fecha) return "";
         const fechaLocal = new Date(fecha);
         return fechaLocal.toLocaleString("es-ES", {
@@ -28,118 +32,221 @@ function Ventas(){
     };
 
 
-
-    //Tabla para mostrar las compras
-        const columns = [
-            { name: "No.", selector: (row) => row.noVenta, sortable: true, width: "100px", wrap: false },
-    
-            { name: "Factura", selector: (row) => row.noFactura, sortable: true, width: "150px", wrap: false },
-    
-            { name: "Fecha", selector: (row) => formatearFecha(row.fechaVenta), sortable: true, width: "150px", wrap: false },
-    
-            { name: "Cliente", selector: (row) => row.nombreCompleto, sortable: true, width: "250px", wrap: false },
-    
-            { name: "Total", selector: (row) => `Q${Number(row.totalVenta).toFixed(2)}`, sortable: true, width: "150px", wrap: false },
-    
-            { name: "Estado", selector: (row) => row.estadoVenta, sortable: true, width: "150px", wrap: false },
-    
-    
-            {
-                name: "Acciones", cell: (row) => <div style={{ display: "flex", gap: "6px" }}>
-                    <Link className="btn btn-sm btn-primary" title="Ver detalles" onClick={() => verDetalles(row.noVenta)}>
-                        <FaEye size={20} />
-                    </Link>
-                    <Link
-                        className={`btn btn-sm ${row.estadoVenta === "Anulada"
-                                ? "btn-danger"
-                                : "btn-danger"
-                            }`}
-                        title={
-                            row.estadoVenta === "Anulada"
-                                ? "Compra anulada"
-                                : "Anular Venta"
-                        }
-                        onClick={() => {
-                            if (row.estadoVenta !== "Anulada") {
-                                anularCompra(row.noCompra);
-                            }
-                        }}
-                        style={{
-                            pointerEvents: row.estadoVenta === "Anulada" ? "none" : "auto",
-                            opacity: row.estadoCompra === "Anulada" ? 0.6 : 1
-                        }}
-                    >
-                        <BsXCircle size={20} />
-                    </Link>
-                </div >
-    
-            }
-        ];
-
-
-
-         const obtenerVentas = async () => {
-                console.log("1️⃣ Componente Ventas montado");
-        
-                try {
-                    const token = localStorage.getItem("token");
-                    console.log("3️⃣ Token obtenido");
-                    console.log(token);
-                    console.log("4️⃣ Antes del fetch");
-        
-                    if (!token) {
-                        console.log("❌ No hay token en localStorage");
-                        setLoading(false);
-                        return;
-                    }
-        
-                    console.log("4️⃣ Antes del fetch");
-        
-                    const response = await fetch(`${import.meta.env.VITE_API_URL}/ventas`, {
-                        method: "GET",
-                        headers: {
-                            "Authorization": `Bearer ${token}`
-                        }
-                    });
-        
-                    console.log("Status:", response.status);
-                    console.log("OK:", response.ok);
-        
-                    if (response.status === 401) {
-                        console.log("❌ Token inválido o expirado");
-        
-                        Swal.fire({
-                            icon: "warning",
-                            title: "Sesión expirada",
-                            text: "Tu sesión ha expirado. Inicia sesión nuevamente.",
-                        }).then(() => {
-                            localStorage.removeItem("token");
-                            navigate("/");
-                        });
-        
-                        setLoading(false);
-                        return;
-                    }
-        
-                    const data = await response.json();
-                    console.log("7️⃣ Data recibida:", data);
-        
-                    setVentas(data);
-        
-                } catch (error) {
-                    console.log("🔥 Error en fetch:", error);
-                } finally {
-                    console.log("8️⃣ Finalizó fetch");
-                    setLoading(false);
+    //Funcion ver detalles
+    const verDetalles = async (idVenta, venta) => {
+        try {
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/ventas/detalles/${idVenta}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    },
                 }
-            }
-        
-            useEffect(() => {
-                obtenerVentas();
-            }, []);
-        
+            );
 
-        //Filtrado por búsqueda
+            if (response.status === 401) {
+                Swal.fire({
+                    icon: "warning",
+                    title: "Sesión expirada",
+                    text: "Inicia sesión nuevamente",
+                }).then(() => {
+                    localStorage.removeItem("token");
+                    navigate("/login");
+                });
+                return;
+            }
+            const data = await response.json();
+            setVentaSeleccionada(venta);
+            setDetallesVenta(data);
+            setMostrarModal(true);
+
+        } catch (error) {
+            console.error("Error al obtener detalles:", error);
+        }
+    };
+
+
+    //Funcion anular venta
+    const anularVenta = async (idVenta) => {
+
+        const confirmacion = await Swal.fire({
+            title: "¿Anularventa?",
+            text: "Esta acción no se puede deshacer",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Sí, anular",
+            cancelButtonText: "Cancelar"
+        });
+
+        if (!confirmacion.isConfirmed) return;
+
+        try {
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/ventas/anular/${idVenta}`,
+                {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    },
+                }
+            );
+
+            if (response.status === 401) {
+                Swal.fire({
+                    icon: "warning",
+                    title: "Sesión expirada",
+                    text: "Inicia sesión nuevamente",
+                }).then(() => {
+                    localStorage.removeItem("token");
+                    navigate("/login");
+                });
+                return;
+            }
+
+            if (!response.ok) {
+                throw new Error("Error al anular la venta");
+            }
+
+            await obtenerVentas();
+
+            Swal.fire({
+                icon: "success",
+                title: "Venta anulada",
+                text: "Venta anulada correctamente",
+                timer: 1500,
+                showConfirmButton: false
+            });
+
+        } catch (error) {
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: error.message
+            });
+        } finally {
+            setLoadingId(null);
+        }
+    }
+
+
+
+
+
+
+
+
+
+    //Tabla para mostrar las ventas
+    const columns = [
+        { name: "No.", selector: (row) => row.noVenta, sortable: true, width: "100px", wrap: false },
+
+        { name: "Factura", selector: (row) => row.noFactura, sortable: true, width: "150px", wrap: false },
+
+        { name: "Fecha", selector: (row) => formatearFecha(row.fechaVenta), sortable: true, width: "150px", wrap: false },
+
+        { name: "Cliente", selector: (row) => row.nombreCompleto, sortable: true, width: "250px", wrap: false },
+
+        { name: "Total", selector: (row) => `Q${Number(row.totalVenta).toFixed(2)}`, sortable: true, width: "150px", wrap: false },
+
+        { name: "Estado", selector: (row) => row.estadoVenta, sortable: true, width: "150px", wrap: false },
+
+
+        {
+            name: "Acciones", cell: (row) => <div style={{ display: "flex", gap: "6px" }}>
+                <Link className="btn btn-sm btn-primary" title="Ver detalles" onClick={() => verDetalles(row.noVenta, row)}>
+                    <FaEye size={20} />
+                </Link>
+                <Link
+                    className={`btn btn-sm ${row.estadoVenta === "Anulada"
+                        ? "btn-danger"
+                        : "btn-danger"
+                        }`}
+                    title={
+                        row.estadoVenta === "Anulada"
+                            ? "Venta anulada"
+                            : "Anular Venta"
+                    }
+                    onClick={() => {
+                        if (row.estadoVenta !== "Anulada") {
+                            anularVenta(row.noVenta);
+                        }
+                    }}
+                    style={{
+                        pointerEvents: row.estadoVenta === "Anulada" ? "none" : "auto",
+                        opacity: row.estadoVenta === "Anulada" ? 0.6 : 1
+                    }}
+                >
+                    <BsXCircle size={20} />
+                </Link>
+            </div >
+        }
+    ];
+
+
+
+    const obtenerVentas = async () => {
+        console.log("1️⃣ Componente Ventas montado");
+
+        try {
+            const token = localStorage.getItem("token");
+            console.log("3️⃣ Token obtenido");
+            console.log(token);
+            console.log("4️⃣ Antes del fetch");
+
+            if (!token) {
+                console.log("❌ No hay token en localStorage");
+                setLoading(false);
+                return;
+            }
+
+            console.log("4️⃣ Antes del fetch");
+
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/ventas`, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            console.log("Status:", response.status);
+            console.log("OK:", response.ok);
+
+            if (response.status === 401) {
+                console.log("❌ Token inválido o expirado");
+
+                Swal.fire({
+                    icon: "warning",
+                    title: "Sesión expirada",
+                    text: "Tu sesión ha expirado. Inicia sesión nuevamente.",
+                }).then(() => {
+                    localStorage.removeItem("token");
+                    navigate("/");
+                });
+
+                setLoading(false);
+                return;
+            }
+
+            const data = await response.json();
+            console.log("7️⃣ Data recibida:", data);
+
+            setVentas(data);
+
+        } catch (error) {
+            console.log("🔥 Error en fetch:", error);
+        } finally {
+            console.log("8️⃣ Finalizó fetch");
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        obtenerVentas();
+    }, []);
+
+
+    //Filtrado por búsqueda
     const text = filterText.trim();
 
     let filteredItems = ventas;
@@ -162,7 +269,7 @@ function Ventas(){
         }
     }
 
-     // Estilos personalizados
+    // Estilos personalizados
     const customStyles = {
         headCells: {
             style: {
@@ -178,14 +285,21 @@ function Ventas(){
     };
 
 
-    return(
+    const totalDetalles = detallesVenta.reduce((acc, item) => {
+        return acc + Number(item.subtotal);
+    }, 0);
+
+    const ventaAnulada = ventaSeleccionada?.estadoVenta === "Anulada";
+
+
+    return (
         <div className="container-custom">
             <div className="p-4">
                 <h4 className="mb-3">Ventas Registradas</h4>
                 <button className="btn btn-primary" onClick={() => navigate("/registrarVentas")}>Crear Venta</button>
             </div>
 
-              {/* Buscador */}
+            {/* Buscador */}
             <input
                 type="text"
                 placeholder="Buscar por número de venta o factura"
@@ -194,7 +308,7 @@ function Ventas(){
                 onChange={(e) => setFilterText(e.target.value)}
             />
 
-             {/* Tabla */}
+            {/* Tabla */}
             <DataTable
                 columns={columns}
                 data={filteredItems}
@@ -215,6 +329,68 @@ function Ventas(){
 
                 noDataComponent="No hay ventas para mostrar"
             />
+
+            <Modal
+                isOpen={mostrarModal}
+                onClose={() => setMostrarModal(false)}
+                title="Detalles de la Compra"
+                size="md"
+                titleSize="18px"
+            >
+                {detallesVenta.length === 0 ? (
+                    <p>No hay detalles para esta compra</p>
+                ) : (
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Producto</th>
+                                <th>Cantidad</th>
+                                <th>Precio</th>
+                                <th>Subtotal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {detallesVenta.map((detalle) => (
+
+                                <tr key={detalle.idDetalle}>
+                                    <td>{detalle.nombreProducto}</td>
+                                    <td>{detalle.cantidad}</td>
+                                    <td>Q{detalle.precio}</td>
+                                    <td>Q{detalle.subtotal}</td>
+
+
+                                    <td>
+                                        <button
+                                            className="btn btn-danger btn-sm"
+                                            title={ventaAnulada ? "Venta anulada" : "Anular producto"}
+                                            onClick={() => {
+                                                if (!ventaAnulada) {
+                                                    anularProducto(detalle);
+                                                }
+                                            }}
+                                            disabled={ventaAnulada}
+                                            style={{
+                                                opacity: ventaAnulada ? 0.5 : 1,
+                                                cursor: ventaAnulada ? "not-allowed" : "pointer"
+                                            }}
+                                        >
+                                            <BsXCircle size={20} />
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                            <tr>
+                                <td colSpan="3" style={{ textAlign: "right", fontWeight: "bold" }}>
+                                    Total:
+                                </td>
+                                <td style={{ fontWeight: "bold" }}>
+                                    Q{totalDetalles.toFixed(2)}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                )}
+            </Modal>
         </div>
     );
 }
