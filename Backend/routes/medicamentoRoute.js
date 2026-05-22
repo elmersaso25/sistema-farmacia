d6987464d370f74736ea2c3cfe08f393b08db455
Elmer Saso Reyes <esasor@miumg.edu.gt>
2026-05-21 21:34:35 -0600

    Se agrego ruta para obtenerCategorias

--- Backend/routes/medicamentoRoute.js
+++ Backend/routes/medicamentoRoute.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { obtenerMedicamentos, obtenerMedicamentosPorId, registrarMedicamentos, actualizarMedicamentos, cambiarEstado, obtenerTotalMedicamentos, buscarMedicamentos, obtenerTotalStockMedicamentos } = require("../controllers/medicamentoController");
+const { obtenerMedicamentos, obtenerMedicamentosPorId, registrarMedicamentos, actualizarMedicamentos, cambiarEstado, obtenerTotalMedicamentos, buscarMedicamentos, obtenerTotalStockMedicamentos, obtenerCategorias } = require("../controllers/medicamentoController");
 
 router.get('/totalMedicamentos', obtenerTotalMedicamentos);
 router.get('/totalStock', obtenerTotalStockMedicamentos);
 router.get('/buscar', buscarMedicamentos);
-router.get('/:id', obtenerMedicamentosPorId);
+router.get('/categorias', obtenerCategorias);
+
 router.get('/', obtenerMedicamentos);
+router.get('/:id', obtenerMedicamentosPorId);
 router.post('/registrar', registrarMedicamentos);
 router.put('/actualizar/:id', actualizarMedicamentos);
 router.patch('/:id/estado', cambiarEstado);
