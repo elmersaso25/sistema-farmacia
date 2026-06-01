a9ee095826d7f63f10e237418be92ca09dfa38a5
Elmer Saso Reyes <esasor@miumg.edu.gt>
2026-06-01 17:27:05 -0600

    Se agregaron las rutas para obtener roles

--- Backend/routes/usuarioRoute.js
+++ Backend/routes/usuarioRoute.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const verificarToken = require('../verificarToken');
 
-const { obtenerUsuarios, obtenerUsuariosPorId, registrarUsuarios, actualizarUsuarios, cambiarEstado, obtenerTotalUsuarios } = require("../controllers/usuarioController");
+const { obtenerUsuarios, obtenerUsuariosPorId, registrarUsuarios, actualizarUsuarios, cambiarEstado, obtenerTotalUsuarios, obtenerRoles } = require("../controllers/usuarioController");
 
 // Todas protegidas con token
 router.get('/totalUsuarios', obtenerTotalUsuarios);
+router.get('/roles', obtenerRoles);
 router.get('/:id', obtenerUsuariosPorId);
 router.get('/', obtenerUsuarios);
 router.post('/registrar', registrarUsuarios);
